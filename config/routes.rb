95ee21815a2ec2dbc2293648c1eccf3a3c89ca27
Chamelek <stetsyshyn.vo@gmail.com>
2019-08-14 23:45:36 +0300

    delete comments routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,3 @@
-# Rails.application.routes.draw do
-#   resources :notes
-#   devise_for :admin_users, ActiveAdmin::Devise.config
-#   ActiveAdmin.routes(self)
-#   devise_for :users, path: '',
-#                      path_names: {
-#                        sign_in: 'login',
-#                        sign_out: 'logout',
-#                        registration: 'signup'
-#                      }, controllers: { sessions: 'sessions', registrations: 'registrations' }
-# end
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
